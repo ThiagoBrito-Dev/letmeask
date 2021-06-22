1612e86540f195e223e06b3fbc2d8fa1703e7ee2
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2021-06-22 20:42:05 -0300

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-// import { Home } from './pages/Home';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
 
 import './styles/global.scss';
 
 function App() {
   return (
-    <NewRoom />
+    <BrowserRouter>
+      <Route exact path="/" component={Home} />
+      <Route path='/rooms/new' component={NewRoom} />
+    </BrowserRouter>
   );
 }
 
